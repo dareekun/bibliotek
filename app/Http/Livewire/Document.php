a37b2a1fe70d00fb5ec8718477f4d83362bf237a
dareekun <dareekun@live.com>
2022-01-24 09:04:41 +0700

    fix dashboard and document list

--- app/Http/Livewire/Document.php
+++ app/Http/Livewire/Document.php
@@ -39,14 +39,14 @@ class Document extends Component
             ->join('location', 'location.id', '=', 'document.location')->join('subcategory', 'subcategory.id', '=', 'document.subcategory')->join('department', 'department.id', '=', 'document.department')
             ->select('document.id as id', 'document.title as title', 'document.no as no', 'document.issuedate as issuedate', 'document.expireddate as expireddate', 'document.created_at as created_at', 
             'users.name as pic', 'document.statusdoc as statusdoc', 'category.desc as category', 'location.code as locode', 'category.code as catcode', 'subcategory.code as subcatcode', 'department.code as deptcode')->where('document.statusdoc', $this->condition)
-            ->where(function($query) {$query->where('document.creator', Auth::user()->id)->orWhere('document.pic', Auth::user()->id);})->get();
+            ->where(function($query) {$query->where('document.creator', Auth::user()->id)->orWhere('document.pic', Auth::user()->email);})->get();
         } elseif (Auth::user()->role == 'pic'){
             $location        = DB::table('department')->where('id', Auth::user()->department)->limit(1)->value('location');
             $this->documents = DB::table('document')->leftJoin('users', 'users.id', '=' ,'document.pic')->join('category', 'category.id', '=' ,'document.category')
             ->join('location', 'location.id', '=', 'document.location')->join('subcategory', 'subcategory.id', '=', 'document.subcategory')->join('department', 'department.id', '=', 'document.department')
             ->select('document.id as id', 'document.title as title', 'document.no as no', 'document.issuedate as issuedate', 'document.expireddate as expireddate', 'document.created_at as created_at',
             'users.name as pic', 'document.statusdoc as statusdoc', 'category.desc as category', 'location.code as locode', 'category.code as catcode', 'subcategory.code as subcatcode', 'department.code as deptcode')->where('document.statusdoc', $this->condition)
-            ->where('document.creator', Auth::user()->id)->get();
+            ->where('document.creator', Auth::user()->email)->get();
         }
         return view('livewire.document');
     }
