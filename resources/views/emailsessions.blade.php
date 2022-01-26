b78beac704a02147723f40b628d7d9ff63f34f5e
dareekun <dareekun@live.com>
2022-01-26 12:07:55 +0700

    add email_job log data

---
+++ resources/views/emailsessions.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+<link rel="stylesheet" href="{{ asset('css/dataTables.bootstrap5.min.css') }}">
+@section('content')
+<div>
+    <div class="row mt-4 me-4">
+        <div class="col-12">
+            <table id="myTable" class="display table border table-striped" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Job Id</th>
+                        <th>Condition</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($logs as $index => $log)
+                    <tr>
+                        <td>{{$index + 1}}</td>
+                        <td>{{$log->refer}}</td>
+                        <td>{{date('Y/m/d - H:i:s', strtotime($log->condition))}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@stop
+@push('scripts')
+<script src="{{ asset('/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/js/dataTables.bootstrap5.min.js') }}"></script>
+<script>
+$(document).ready(function() {
+    $('#myTable').DataTable();
+});
+</script>
+@endpush
