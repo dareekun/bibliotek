201007b8226b38e6abafe651db278f269a360476
Mada Baskoro <70L3790@intranet.co.id>
2022-04-19 15:22:54 +0700

    fix layout 3

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -11,24 +11,18 @@
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
             <div class="form-group position-relative has-icon-left mb-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="form-control form-control-xl" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-                <div class="form-control-icon">
-                    <i class="bi bi-envelope"></i>
-                </div>
             </div>
 
             <div class="form-group position-relative has-icon-left mb-4">
-                <input id="password" type="password" class="form-control form-control-xl" name="password" required autocomplete="new-password">
-                <div class="form-control-icon">
-                    <i class="bi bi-key"></i>
-                </div>
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="form-control form-control-xl" type="password" name="password" required autocomplete="new-password" />
             </div>
             
             <div class="form-group position-relative has-icon-left mb-4">
-                <input id="password" type="password" class="form-control form-control-xl" name="password" required autocomplete="new-password">
-                <div class="form-control-icon">
-                    <i class="bi bi-key"></i>
-                </div>
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" class="form-control form-control-xl" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
             <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Send Password Reset Link</button>
         </form>
